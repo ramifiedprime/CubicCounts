c8a88f666f5b5b1cf88620aa4da8261236536279
ramifiedprime <Ross.Paterson95@googlemail.com>
2026-05-27 15:47:22 +0100

    subdivided library files

--- c_code/CCFCCFPRP.h
+++ c_code/CCFCCFPRP.h
@@ -2,22 +2,6 @@
 #define CCFCCFPRP_H
 #include<stdio.h>
 
-
-/**
- * @brief Recovers cubic fields of bounded discriminant (algorithm 5.7).
- * 
- * Implements Belabas' algorithm, printing results to a file.  The printed
- * results take the form of lines:
- *      a,b,c,d\n
- * where ax^3+bx^2y+cxy^2+dy^3 is a reduced binary cubic form corresponding 
- * to a complex cubic field.  Obtains all such forms of discriminant at most X.
- * 
- * @param X Discriminant bound for cubic fields.
- * @param *fptr file to output results to.
- * @param verbose verbosity flag for bug-tests.
- * @return 0
- */
-int CCFCCF(long X, FILE *fptr, int verbose);
 /**
  * @brief Recovers cubic fields of bounded product of ramified primes (adapts algorithm 5.7).
  * 
